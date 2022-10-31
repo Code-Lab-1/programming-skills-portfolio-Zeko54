c8bfd781e0b7450943a36710f0f86f1875767b15
Zoraiz Mudassar <114096672+Zeko54@users.noreply.github.com>
2022-10-31 19:22:46 +0400

    aa

---
+++ Chapter 3- Data Structures/Exercises/excercise 4 guest list.py
@@ -0,0 +1,4 @@
+Invitation = ['Ali' , 'Armash' , 'Ahmed']
+print(Invitation[0] + " I would like to invite you to dinner")
+print(Invitation[1] + " I would like to invite you to dinner")
+print(Invitation[2] + " I would like to invite you to dinner")
