080f42bd44a3942d4363331a20d3b4f3ef4b2096
Zoraiz Mudassar <114096672+Zeko54@users.noreply.github.com>
2022-10-31 19:20:21 +0400

    chapter 3 all exercises

---
+++ Chapter 3- Data Structures/Exercises/excercise 1 Names.py
@@ -0,0 +1,7 @@
+names=['Mohammed','Ali','Muntazir','Armash','Hamad']
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
---
+++ Chapter 3- Data Structures/Exercises/excercise 3 your own list.py
@@ -0,0 +1,5 @@
+Vehicles= ['Motorcycle:', 'Cars:' ,'Cycles:' , 'Bus:' ]
+print(Vehicles[0] + " I love motorcycles they are fun to ride ")
+print(Vehicles[1] + " Cars are the best of Transportation")
+print(Vehicles[2] + " Cycles are fun to ride")
+print(Vehicles[3] + " I dont like Buses")
