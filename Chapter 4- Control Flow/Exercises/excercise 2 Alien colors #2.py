5854b74cca2936ffe3076707292fdf7bb264c978
Zoraiz Mudassar <114096672+Zeko54@users.noreply.github.com>
2022-10-31 19:27:42 +0400

    chapter 4 all exercises

---
+++ Chapter 4- Control Flow/Exercises/excercise 2 Alien colors #2.py
@@ -0,0 +1,12 @@
+alien_color = 'green'                                                  #if block
+if alien_color == 'green':
+  print('You earned 5 points for killing the alien')
+else:
+  print('You earned 10 points')
+
+
+alien_color = 'red'                                                  #else block
+if alien_color == 'green':
+  print('You earned 5 points for killing the alien')
+else:
+  print('You earned 10 points')
