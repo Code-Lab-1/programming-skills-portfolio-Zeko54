4d72f10c799f13841c03da695bfa488dbde39cb4
Zoraiz Mudassar <114096672+Zeko54@users.noreply.github.com>
2022-10-31 19:33:41 +0400

    chapter 5 all exercises

---
+++ Chapter 5- Dictionaries/Exercises/excercise 1 person.py
@@ -0,0 +1,2 @@
+person = {"first_name" : "Zoraiz" , "last_name" : "Mudassar" , "age" : 19 , "city" : "sharjah"}
+print(person)
---
+++ Chapter 5- Dictionaries/Exercises/exercise 2 glossary.py
@@ -0,0 +1,22 @@
+glossary = {
+  'Indentation': "Indentation refers to the spaces at the beginning of a code line", 
+  'String': "A series of characters",
+  'list': "A collection of items in a particular order", 
+  'Set': "A set is an unordered, and unchangeable, collection", 
+  'Comments': "Comments are code lines that will not be executed",
+    }
+
+word = 'Indentation'
+print(word.title() + " : " + glossary[word])
+
+word = 'String'
+print(word.title() + " : " + glossary[word])
+
+word = 'list'
+print(word.title() + " : " + glossary[word])
+
+word = 'Set'
+print(word.title() + " : " + glossary[word])
+
+word = 'Comments'
+print(word.title() + " : " + glossary[word])
