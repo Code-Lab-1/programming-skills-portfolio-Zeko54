e608dd7ff0b5b8a0ac1f0cf0000893d325ab7b21
Zoraiz Mudassar <114096672+Zeko54@users.noreply.github.com>
2022-11-02 09:18:43 +0400

    aa

--- Chapter 1- Getting Started with Python/Excercise 1 Print strings.py
+++ Chapter 1- Getting Started with Python/Excercise 1 Print strings.py
@@ -3,4 +3,4 @@ print("Twinkle, twinkle, little star,\n"
             "Up above the world so high,\n"
             "Like a diamond in the sky.\n"
       "Twinkle,twinkle, little star,\n"
-          "How I wonder what you are")
+          "How I wonder what you are")                     
